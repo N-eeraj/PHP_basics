e016dbae6d2b842a16f3bb18953e3928fb74ab32
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-02 21:21:17 +0530

    added a simple form with php

---
+++ form1.php
@@ -0,0 +1,11 @@
+<?php
+    echo "Welcome " . $_POST["name"];
+    echo "<br>";
+    echo "Your User-Id: " . $_POST["mail"];
+    echo "<br>";
+    echo "Your Age: " . $_POST["age"];
+    echo "<br>";
+    echo "Your Password: " . $_POST["password"];
+    echo "<br>";
+    echo "Your Gender: " . $_POST["gender"];
+?>
