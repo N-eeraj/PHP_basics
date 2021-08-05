ce5845859a00cc33691d1b091b8bc96d4e4c5169
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-05 23:22:54 +0530

    added 2 patterns

---
+++ patterns.php
@@ -0,0 +1,19 @@
+<?php
+
+    for($i = 1; $i < 8; $i++)
+    {
+        for($j = 0; $j < $i; $j++)
+            echo "*";
+        echo "<br>";
+    }
+    echo "<br><br>";
+
+    for($i = 7; $i > 0; $i--)
+    {
+        for($j = 0; $j < $i; $j++)
+            echo "*";
+        echo "<br>";
+    }
+    echo "<br><br>";
+
+?>
